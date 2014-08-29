584d840a5388ce5e50c0729fae176b92863ef4f2
tomohiro-maruyama <tomohiro.maruyama@ctc-g.co.jp>
2014-08-29 15:39:06 +0900

    Modify imagebuild.sh and README of the pi-montecarlo application
    Change-Id: Id857828cd7049c853686205bb542a2f17a7d56ac

--- tools/sample-apps/pi-montecarlo/imagebuild.sh
+++ tools/sample-apps/pi-montecarlo/imagebuild.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env sh
 
-APP_PATH=~/rack/tools/sample-apps/pi-montecarlo
-yum install git -y
+yum update -y
 easy_install pip
-pip install -U setuptools
-git clone https://github.com/stackforge/rack.git ~/rack
-pip install -r $APP_PATH/requirement.txt
-
+pip install -r ./requirements.txt
 curl -o /usr/bin/jq http://stedolan.github.io/jq/download/linux64/jq
 chmod +x /usr/bin/jq
-chmod +x $APP_PATH/app/*.sh
